1ab6b1edcc6486c8c727ccd38df5536c5b2c87d1
Clement <56961627+Verboom30@users.noreply.github.com>
2025-01-26 21:51:03 +0100

    holonome.cpp

--- lib/Holonome/holonome.hpp
+++ lib/Holonome/holonome.hpp
@@ -41,71 +41,73 @@ class Holonome
         SerialTMC * SWSerialHolonome = nullptr;
 
         bool    setupSteppers(void);
-        // void    getPosition(void);
-        // float   getPositionX(void);
-        // float   getPositionY(void);
-        // float   getPosCibleX(void);
-        // float   getPosCibleY(void);
-        // float   getAlpha(void);
-        // float   getSpeedX(void);
-        // float   getSpeedY(void);
-        // float   getSpeedAlpha(void);
+        void    getPosition(void);
+        float   getPositionX(void);
+        float   getPositionY(void);
+        float   getPosCibleX(void);
+        float   getPosCibleY(void);
+        float   getAlpha(void);
+        float   getSpeedX(void);
+        float   getSpeedY(void);
+        float   getSpeedAlpha(void);
         
     
-        // void  setPosition(int positionX, int positionY, int Alpha);
-        // void  setPositionZero(void);
-        // bool  stopped(void);
-        // bool  waitAck(void);
-        // bool  PosCibleDone(void);
+        void  setPosition(int positionX, int positionY, int Alpha);
+        void  setPositionZero(void);
+        bool  stopped(void);
+        bool  waitAck(void);
+        bool  PosCibleDone(void);
 
-        // void  HolonomeSpeed(int SpeedX, int SpeedY, int W);
-        // void  goesTo(int positionX, int positionY, int Alpha); 
-        // void  move(int positionX, int positionY, int Alpha); 
-        // void  stop(void);
-        // void  run(void);
-        // float getSpeedA(void);
-        // float getSpeedB(void);
-        // float getSpeedC(void);
-        // int getPosA(void);
-        // int getPosB(void);
-        // int getPosC(void);
-        // int getStepA(void);
-        // int getStepB(void);
-        // int getStepC(void);
+        void  HolonomeSpeed(int SpeedX, int SpeedY, int W);
+        void  goesTo(int positionX, int positionY, int Alpha); 
+        void  move(int positionX, int positionY, int Alpha); 
+        void  stop(void);
+        void  run(void);
+        float getSpeedA(void);
+        float getSpeedB(void);
+        float getSpeedC(void);
+        int getPosA(void);
+        int getPosB(void);
+        int getPosC(void);
+        int getStepA(void);
+        int getStepB(void);
+        int getStepC(void);
+        Thread routine;
+        void routine_holonome(void);
         
     private : 
-        // float _acc;                             //Acceleration [step/s²]
-        // float _dec;                             //Decceleration [step/s²]
-        // float _spd;                             //Speed [step/s]
-        // float _Alpha;
-        // float _positionX;
-        // float _positionY;
-        // float _cibleposX;
-        // float _cibleposY;
-        // float _positionX_Save;
-        // float _positionY_Save;
-        // float _Alpha_Save;
-        // float _MoveAlpha; 
-        // float _MovepositionX;
-        // float _MovepositionY;
-        // float _SpeedX;
-        // float _SpeedY;
-        // float _SpeedAlpha;
+        float _acc;                             //Acceleration [step/s²]
+        float _dec;                             //Decceleration [step/s²]
+        float _spd;                             //Speed [step/s]
+        float _Alpha;
+        float _positionX;
+        float _positionY;
+        float _cibleposX;
+        float _cibleposY;
+        float _positionX_Save;
+        float _positionY_Save;
+        float _Alpha_Save;
+        float _MoveAlpha; 
+        float _MovepositionX;
+        float _MovepositionY;
+        float _SpeedX;
+        float _SpeedY;
+        float _SpeedAlpha;
         
-        // string _Cmd;
-        // bool _AckStpA;
-        // bool _AckStpB;
-        // bool _AckStpC;
+        string _Cmd;
+        bool _AckStpA;
+        bool _AckStpB;
+        bool _AckStpC;
 
         // Thread StepperA_thread;
         // Thread StepperB_thread;
         // Thread StepperC_thread;
-        // Thread routine;
+      
         
         // // void routine_stepperA(void);
         // // void routine_stepperB(void);
         // // void routine_stepperC(void); 
-        // void routine_holonome(void);
+         
     protected:
         //***********************************/************************************
         //                          Protected Methods                           //
