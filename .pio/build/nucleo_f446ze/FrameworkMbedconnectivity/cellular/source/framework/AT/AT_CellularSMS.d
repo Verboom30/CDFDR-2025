7abf89ed112795db4610b09acc7e3762c383567a
Clement <56961627+Verboom30@users.noreply.github.com>
2025-04-16 01:36:49 +0200

    delete file

--- .pio/build/nucleo_f446ze/FrameworkMbedconnectivity/cellular/source/framework/AT/AT_CellularSMS.d
+++
@@ -1,3 +0,0 @@
-.pio\build\nucleo_f446ze\FrameworkMbedconnectivity\cellular\source\framework\AT\AT_CellularSMS.o: \
- C:\Users\cleme\.platformio\packages\framework-mbed\connectivity\cellular\source\framework\AT\AT_CellularSMS.cpp \
- .pio\build\nucleo_f446ze/mbed_config.h
